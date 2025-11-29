319878aa298cf4b4259938962761cceba098f099
tahleel14 <tehleel.nisar2024@lpu.in>
2025-11-29 14:06:27 +0530

    Add function to find largest element in array
    Implement function to find the largest element in an array.

--- day1/solution.cpp
+++ day1/solution.cpp
@@ -1,1 +1,15 @@
+//day1: Largest element in the array 
+//problem link : https://www.geeksforgeeks.org/problems/largest-element-in-array4009/0
+//platform :Geeks for Geeks 
+// User function template for C
+int largest(int arr[], int n) {
+    // Code Here
+    int max = arr [0]; 
+    for ( int i = 0 ; i < n ; i ++ ){ 
+        if ( arr[i]>max ){ 
+            max = arr[i]; 
+        }
+    } 
+    return max ; 
+}
 
