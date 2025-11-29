bc03db7bb2a92ef20b759903076a5fb2ef424d54
tahleel14 <tehleel.nisar2024@lpu.in>
2025-11-29 14:28:30 +0530

    Update solution.cpp

--- day1/solution.cpp
+++ day1/solution.cpp
@@ -1,15 +1,18 @@
 //day1: Largest element in the array 
 //problem link : https://www.geeksforgeeks.org/problems/largest-element-in-array4009/0
 //platform :Geeks for Geeks 
-// User function template for C
-int largest(int arr[], int n) {
-    // Code Here
-    int max = arr [0]; 
-    for ( int i = 0 ; i < n ; i ++ ){ 
-        if ( arr[i]>max ){ 
-            max = arr[i]; 
-        }
-    } 
-    return max ; 
-}
-
+// User function template for C++
+class Solution {
+  public:
+    int largest(vector<int> &arr) {
+        int max = arr[0]; 
+        for ( int i = 0 ; i< n ; i++){ 
+            if ( arr[i ]> max ){ 
+                max = arr [i]; 
+            } 
+        } 
+        return max ; 
+        
+        
+    }
+};
