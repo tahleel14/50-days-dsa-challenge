1fca0812dbbe3adc4b24d922760e2744b3c3dff1
tahleel14 <tehleel.nisar2024@lpu.in>
2025-12-24 21:10:28 +0530

    Create solution.cpp

---
+++ day25/solution.cpp
@@ -0,0 +1,15 @@
+// 🚀 Day 25 – Tower of Hanoi (Recursion)
+
+// 🧑‍💻 Problem: Tower of Hanoi
+// 🔗 GFG Link:
+// https://www.geeksforgeeks.org/problems/tower-of-hanoi-1587115621/1
+class Solution {
+  public:
+    int towerOfHanoi(int n, int from, int to, int aux) {
+        // code here
+     
+        return (1 << n) - 1;   // 2^n - 1
+    }
+};
+
+  
